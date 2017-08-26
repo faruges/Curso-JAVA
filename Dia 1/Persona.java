c189bb2a9c4d2f3bb0cbed7e8f08e5c74bd73f76
Felipe Segura <fseguraf@hotmail.com>
2017-08-26 15:03:50 -0500

    Mi primer Commit

---
+++ Dia 1/Persona.java
@@ -0,0 +1,50 @@
+
+public class Persona {
+
+ String name;
+ String sexo;
+ int edad;
+
+ public void caminar(){
+	System.out.println(name + "Esta caminando");
+}
+ public void estudiar(){
+	System.out.println(name + "Esta estudiando");
+	}
+ public void  comer(String comida){
+	System.out.println(name + "Esta Comiendo" + comida);
+	} 
+ public void pedirCerveza(){
+
+ if(edad>= 18){
+	System.out.print("Dar cerveza");
+   }else{
+	System.out.print("No tienes edad para tomar");	
+ 
+    }
+  }
+
+}
+
+class Curso {
+
+ public satatic void main(String args[]){
+ Persona juan = new Persona();
+ juan.name="JUAN PEREZ";
+ juna.sexo="M";
+ juan.edad=22;
+ juan.caminar();
+ juan.estudiar();
+ juan.comer("Torta Cubana + Guacamole");
+ 
+ Persona bety= new Persona();
+ bety.name="Beatriz";
+ bety.sexo="F";
+ bety.edad=18;
+ bety.caminar();
+ bety.estudiar();
+
+
+
+ }
+}
---
+++ Dia 1/_C.java
@@ -0,0 +1,8 @@
+public class _C {
+
+	public static void main(String[] ipn){	
+
+System.out.println(ipn[0] + "Experto en Java");
+
+	}
+}
